ad0c3061ad0021093e6e4bdb70894619bbe1e114
Max Kurapov <maxkurapov@gmail.com>
2019-03-11 19:27:46 -0600

    start of game controller logic

---
+++ Spark AR/Assets/Components/Core/Scripts/Planet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    public string name;
+    public bool isCollected = false;
+    public Question questionData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Spark AR/Assets/Components/Core/Scripts/Question.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Question : MonoBehaviour
+{
+    public string questionText;
+    public string correctAnswerReason;
+    public List<Answer> answers;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
